cc2dfefb55854d6321fb32501602f22dd14c87cc
mwi <mwi@youngsolutions.ch>
2014-11-04 17:37:09 +0100

    adding foundation (incl template & icon font) & font awesome, started with layout

--- app/views/_layout/default.php
+++ app/views/_layout/default.php
@@ -1,5 +1,238 @@
-<html>
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html lang="en" >
 
-<?= $content ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <title>
+        <?php if (isset($title)): ?>
+            <?= $title ?>
+        <?php else: ?>
+            SCAM
+        <?php endif ?>
+    </title>
+
+    <link rel="stylesheet" href="font/foundation-icons/foundation-icons.css" />
+    <link rel="stylesheet" href="font/font-awesome/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/foundation.css">
+
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+<div class="row">
+<div class="large-12 columns">
+
+    <div class="row">
+        <div class="large-12 columns">
+            <nav class="top-bar" data-topbar>
+                <ul class="title-area">
+
+                    <li class="name">
+                        <h1><a href="#">
+                                <i class="fi-shield"></i>
+                                <span class="title">
+                                    <span class="first">S</span>cammers
+                                    <span class="first">C</span>an't
+                                    <span class="first">A</span>cquire
+                                    <span class="first">M</span>oney
+                                </span>
+                            </a></h1>
+                    </li>
+                    <li class="toggle-topbar menu-icon">
+                        <a href="#"><span>menu</span></a>
+                    </li>
+                </ul>
+                <?php if(true): ?>
+                <section class="top-bar-section">
+                    <ul class="right">
+                        <li class="divider"></li>
+                        <li class="has-dropdown not-click">
+                            <a href="#">Main Item 1</a>
+                            <ul class="dropdown">
+                                <li><label>Section Name</label></li>
+                                <li class="has-dropdown not-click">
+                                    <a class="" href="#">Has Dropdown, Level 1</a>
+                                    <ul class="dropdown">
+                                        <li>
+                                            <a href="#">Dropdown Options</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Dropdown Options</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Level 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Subdropdown Option</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Subdropdown Option</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Subdropdown Option</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">Dropdown Option</a>
+                                </li>
+                                <li>
+                                    <a href="#">Dropdown Option</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li><label>Section Name</label></li>
+                                <li>
+                                    <a href="#">Dropdown Option</a>
+                                </li>
+                                <li>
+                                    <a href="#">Dropdown Option</a>
+                                </li>
+                                <li>
+                                    <a href="#">Dropdown Option</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a href="#">See all →</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#">Main Item 2</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li class="has-dropdown not-click">
+                            <a href="#">Main Item 3</a>
+                            <ul class="dropdown">
+                                <li>
+                                    <a href="#">Dropdown Option</a>
+                                </li>
+                                <li>
+                                    <a href="#">Dropdown Option</a>
+                                </li>
+                                <li>
+                                    <a href="#">Dropdown Option</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a href="#">See all →</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="divider"></li>
+                        <li class="has-form">
+                            <a href="#" class="button alert logout">
+                                Logout
+                                <i class="fa fa-sign-out"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </section>
+                <?php endif ?>
+            </nav>
+        </div>
+    </div>
+    <div class="row body">
+
+        <div class="large-4 small-12 columns">
+            <h3 class="subheader"></h3>
+
+            <div class="hide-for-small panel">
+                <h3 class="subheader">Be safe using <abbr title="Scammer Can't Acquire Money">SCAM.</h3>
+                <h5 class="subheader">
+                    Don't get scammed - use our highly secure market that never stores any of your valuable bitcoins.
+                </h5>
+            </div><a href="#">
+                <div class="panel callout radius">
+                    <h6>99&nbsp; items in your <?= $content ?></h6>
+                </div></a>
+        </div>
+
+        <div class="large-8 columns">
+            <div class="row content">
+                <div class="large-4 small-6 columns">
+                    <img src="http://placehold.it/1000x1000&amp;text=Thumbnail">
+                    <div class="panel">
+                        <h5>Item Name</h5>
+                        <h6 class="subheader">$000.00</h6>
+                    </div>
+                </div>
+                <div class="large-4 small-6 columns">
+                    <img src="http://placehold.it/500x500&amp;text=Thumbnail">
+                    <div class="panel">
+                        <h5>Item Name</h5>
+                        <h6 class="subheader">$000.00</h6>
+                    </div>
+                </div>
+                <div class="large-4 small-6 columns">
+                    <img src="http://placehold.it/500x500&amp;text=Thumbnail">
+                    <div class="panel">
+                        <h5>Item Name</h5>
+                        <h6 class="subheader">$000.00</h6>
+                    </div>
+                </div>
+                <div class="large-4 small-6 columns">
+                    <img src="http://placehold.it/500x500&amp;text=Thumbnail">
+                    <div class="panel">
+                        <h5>Item Name</h5>
+                        <h6 class="subheader">$000.00</h6>
+                    </div>
+                </div>
+                <div class="large-4 small-6 columns">
+                    <img src="http://placehold.it/500x500&amp;text=Thumbnail">
+                    <div class="panel">
+                        <h5>Item Name</h5>
+                        <h6 class="subheader">$000.00</h6>
+                    </div>
+                </div>
+                <div class="large-4 small-6 columns">
+                    <img src="http://placehold.it/500x500&amp;text=Thumbnail">
+                    <div class="panel">
+                        <h5>Item Name</h5>
+                        <h6 class="subheader">$000.00</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="large-12 columns">
+                    <div class="panel">
+                        <div class="row">
+                            <div class="large-2 small-6 columns"><img src="http://placehold.it/300x300&amp;text=Site%20Owner"></div>
+                            <div class="large-10 small-6 columns">
+                                <strong>This Site Is Managed By</strong>
+                                <hr>
+                                Risus ligula, aliquam nec fermentum vitae, sollicitudin eget urna. Donec dignissim nibh fermentum
+                                odio ornare sagittis
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="row">
+        <div class="large-12 columns">
+            <hr>
+            <div class="row">
+                <div class="large-6 columns">
+                    <p>© Matthias Winzeler @ Berne University of Applied Sciences.</p>
+                </div>
+                <div class="large-6 columns">
+                    <ul class="inline-list right">
+                        <li>
+                            <a href="https://github.com/MatthiasWinzeler/scam" target="_blank">Github repo</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+</div>
+</body>
 </html>
