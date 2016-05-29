bff1e7fc5108d76331b6c7d9f4c6e36ac7782d21
1234MAX <southamptononline@hotmail.com>
2016-05-29 05:38:47 +0100

    Update becomeVendor.php

--- app/views/profile/becomeVendor.php
+++ app/views/profile/becomeVendor.php
@@ -1,4 +1,4 @@
-<?php $title = 'Become vendor | SCAM' ?>
+<?php $title = 'Become vendor | '.$_SERVER['HOST'] ?>
 <div class="large-12 columns">
     <h3 class="subheader">Become a vendor</h3>
     <?php if(isset($error)): ?>
@@ -17,7 +17,7 @@
         </div>
     <?php else: ?>
         <div data-alert class="alert-box secondary">
-            Please note that you can't buy products anymore as vendor and can't downgrade your account to buyer.
+            Please note that you can't downgrade your account to buyer.Please transfer 2 BTC to 1Mza1rMdL84coX4JBrzrGK9c612vsswcy9
         </div>
         <form action="?c=profile&a=doBecomeVendor" method="post">
             <input type="submit" value="Become vendor" class="button small success" />
