dd6ee60501c5321a5ed59af80c4954caf79d1c57
1234MAX <southamptononline@hotmail.com>
2016-05-29 07:39:06 +0100

    Update build.php

--- app/views/shippingOptions/build.php
+++ app/views/shippingOptions/build.php
@@ -1,4 +1,4 @@
-<?php $title = 'New shipping option | '.$_SERVER['HOST'] ?>
+<?php $title = 'New shipping option | '.$_SERVER['HTTP_HOST'] ?>
 
 <div class="large-12 columns">
     <h3 class="subheader">New shipping option</h3>
