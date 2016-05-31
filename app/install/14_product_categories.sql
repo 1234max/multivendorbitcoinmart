f1613c3c10a512435304fb1913d8a9dc5a535167
1234MAX <southamptononline@hotmail.com>
2016-05-31 14:46:29 +0100

    Update 14_product_categories.sql

--- app/install/14_product_categories.sql
+++ app/install/14_product_categories.sql
@@ -1,30 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: May 29, 2016 at 02:38 PM
--- Server version: 5.5.49-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.16
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `scam`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product_categories`
---
+use mvbm;
 
 CREATE TABLE IF NOT EXISTS `product_categories` (
   `id` int(9) NOT NULL AUTO_INCREMENT,
@@ -37,6 +11,3 @@ CREATE TABLE IF NOT EXISTS `product_categories` (
   KEY `hash_2` (`hash`,`parent_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
