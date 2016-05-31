766c0d50446602638a03f71588ac7a58b9221bba
1234MAX <southamptononline@hotmail.com>
2016-05-31 14:56:01 +0100

    Update create_tables.sql

--- app/install/create_tables.sql
+++ app/install/create_tables.sql
@@ -1,30 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: May 29, 2016 at 02:49 PM
--- Server version: 5.5.49-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.16
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `som`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bitcoin_payments`
---
+Create database mvbm2;
 
 CREATE TABLE IF NOT EXISTS `bitcoin_payments` (
   `address` varchar(35) NOT NULL,
