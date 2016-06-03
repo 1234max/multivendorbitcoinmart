8b3813201d2714771983f303a347c59e938b19ce
1234MAX <southamptononline@hotmail.com>
2016-06-03 14:54:43 +0100

    Delete 03_create_shipping_options_table.sql

--- app/install/03_create_shipping_options_table.sql
+++
@@ -1,9 +0,0 @@
-USE mvbm;
-
-CREATE TABLE IF NOT EXISTS `shipping_options` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(100) NOT NULL,
-  `price` decimal(65,30) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4
