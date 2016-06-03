439e76ae4c678f3f25755e3d120156a4e8f4ab20
1234MAX <southamptononline@hotmail.com>
2016-06-03 14:57:09 +0100

    Delete 07_create_bitcoin_transactions_table.sql

--- app/install/07_create_bitcoin_transactions_table.sql
+++
@@ -1,8 +0,0 @@
-USE mvbm;
-
-
-CREATE TABLE IF NOT EXISTS `bitcoin_transactions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `tx_id` varchar(64) NOT NULL UNIQUE,
-  `raw_tx` text NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4
