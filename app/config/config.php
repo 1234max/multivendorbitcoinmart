48a6329301783ad43b56762f69d6f0e9806181eb
1234MAX <southamptononline@hotmail.com>
2016-06-03 15:10:13 +0100

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,29 +1,6 @@
 
 <?php
 
-/**
- * holds all configuration data.
- *
- * reasons the error reporting is that way: @see http://www.phptherightway.com/#error_reporting
- */
-
-# dev env
-if(true) {
-    error_reporting(E_ALL);
-    ini_set("display_errors", 1);
-    ini_set("display_startup_errors", 1);
-    ini_set("log_errors", 1);
-    define('Commission', '5');
-    define('BITCOIND_URL', 'http://bitcoinrpcuser:bitcoinrpcpasswd@127.0.0.1:8332');
-    define('DB_HOST', '127.0.0.1');
-    define('DB_NAME', 'mvbm');
-    define('DB_USER', 'mvbm');
-    define('DB_PASS', '');
-    define('PRODUCTION', false);
-    date_default_timezone_set('UTC');
-}
-# prod env
-else {
     error_reporting(E_ALL);
     ini_set("display_errors", 0);
     ini_set("display_startup_errors", 0);
@@ -36,4 +13,3 @@ else {
     define('DB_PASS', ' ');
     define('PRODUCTION', true);
     date_default_timezone_set('UTC');
-}
