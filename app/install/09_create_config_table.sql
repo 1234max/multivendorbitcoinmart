20f3367d729aeffcfd450f4e195afd87896ca1d6
1234MAX <southamptononline@hotmail.com>
2016-06-03 14:57:39 +0100

    Delete 09_create_config_table.sql

--- app/install/09_create_config_table.sql
+++
@@ -1,8 +0,0 @@
-USE mvbm;
-
-
-CREATE TABLE IF NOT EXISTS `config` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(100) NOT NULL UNIQUE,
-  `value` text
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4
