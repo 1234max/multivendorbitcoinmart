5f678632e222fc3c8a49d1cb4e8981c6c7e277b2
1234MAX <southamptononline@hotmail.com>
2016-06-03 15:12:43 +0100

    Update config.default.php

--- app/config/config.default.php
+++ app/config/config.default.php
@@ -1,38 +1,39 @@
 <?php
 
-/**
- * holds all configuration data.
- *
- * reasons the error reporting is that way: @see http://www.phptherightway.com/#error_reporting
- */
 
 # dev env
 if(true) {
+    
+    
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
     ini_set("display_startup_errors", 1);
     ini_set("log_errors", 1);
     define('Commission', '5');
-    define('BITCOIND_URL', 'http://bitcoinrpc:1234@127.0.0.1:28332');
+    define('BITCOIND_URL', 'http://bitcoinrpcusr:bitcoinrpcpasswd@127.0.0.1:8332');
     define('DB_HOST', '127.0.0.1');
-    define('DB_NAME', 'scam');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
+    define('DB_NAME', 'mvbm');
+    define('DB_USER', 'mvbm');
+    define('DB_PASS', ' ');
     define('PRODUCTION', false);
     date_default_timezone_set('UTC');
+    
 }
 # prod env
 else {
+    
+    
     error_reporting(E_ALL);
     ini_set("display_errors", 0);
     ini_set("display_startup_errors", 0);
     ini_set("log_errors", 1);
     define('Commission', '5');
-    define('BITCOIND_URL', 'http://bitcoinrpc:HIGHSECURE@127.0.0.1:28332');
+    define('BITCOIND_URL', 'http://bitcoinrpcusr:bitcoinrpcpasswd@127.0.0.1:8332');
     define('DB_HOST', '127.0.0.1');
-    define('DB_NAME', 'scam');
-    define('DB_USER', 'not-root');
-    define('DB_PASS', 'mysql');
+    define('DB_NAME', 'mvbm');
+    define('DB_USER', 'mvbm');
+    define('DB_PASS', ' ');
     define('PRODUCTION', true);
     date_default_timezone_set('UTC');
+    
 }
