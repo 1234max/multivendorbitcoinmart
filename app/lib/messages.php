6ac604a1db756043a2753e8cd17a60fedda342a5
1234MAX <southamptononline@hotmail.com>
2016-06-10 13:13:43 +0000

    Create messages.php

---
+++ app/lib/messages.php
@@ -0,0 +1,164 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Bw_messages {
+	/**
+	 * Message Password
+	 * 
+	 * This is used to store the users message password.
+	 */
+	protected $message_password;
+	public $CI;
+	
+	/**
+	 * This is set by the site configuration, determines whether RSA encryption
+	 * of message is done.
+	 */
+	public $encrypt_private_messages;
+	
+	/**
+	 * Constructor
+	 * 
+	 * Load the CodeIgniter framework, and the OpenSSL/GPG libraries,
+	 * and the users model.
+	 */
+	public function __construct() {
+		$this->CI = &get_instance();
+		$this->CI->load->library('openssl');
+		$this->CI->load->library('gpg');
+		$this->CI->load->model('users_model');
+	}	
+	
+	/**
+	 * Prepare Input
+	 * 
+	 * Prepares a message for storage in the database. If $system is NULL,
+	 * then the message information is taken from POST data (ie, submitted
+	 * using the messages form). Otherwise, the message is taken from the
+	 * system array.
+	 * 
+	 * Checks if the message is encrypted, or if the vendor has enabled
+	 * server side encryption of GPG messages.
+	 * 
+	 * Prepares the content columns data - a JSON array containing the
+	 * message, subject, and sender ID. If encrypted private messages are
+	 * enabled, this message content will be encrypted with the receipients
+	 * RSA key to securely store the data.
+	 * Finally content is base64 encoded.
+	 * 
+	 * @param	array	$data
+	 * @param	array	$system
+	 */
+	public function prepare_input($data, $system = NULL) {
+		
+		// If the message isn't being sent by the system, take info from POST.
+		if($system == NULL) {
+			$username = $this->CI->input->post('recipient');
+			$subject = $this->CI->input->post('subject');
+			if($subject == '')
+				$subject = '[no subject]';
+			$message = $this->CI->input->post('message');
+			$remove_on_read = ($this->CI->input->post('delete_on_read') == '1') ? '1' : '0';
+		} else if(is_array($system)) {
+			// If it's a system message, load from the array.
+			$username = $system['username'];
+			$subject = $system['subject'];
+			$message = $system['message'];
+			$remove_on_read = '0';
+		} 
+		
+		
+		$from = $data['from'];
+		
+		// Load the account the message is being sentto.
+		$to = $this->CI->users_model->get(array('user_name' => $username));
+				
+		$content = array('from' => $from,
+						'subject' => $subject,
+						'message' => $message);
+						
+		// Check if the message is encrypted.
+		$encrypted = ((stripos($message, '-----BEGIN PGP MESSAGE-----') !== FALSE) && (stripos($message, '-----END PGP MESSAGE-----') !== FALSE)) ? '1' : '0' ;
+		
+		// If the message isn't already encrypted with PGP..
+		if($encrypted == '0') {
+			// If the sender has requested it, or the recipient has forced it,
+			// encrypt the message with the recipients public key.
+			if( ($this->CI->input->post('pgp_encrypt') == '1') ||
+			    ($to['force_pgp_messages'] == '1') ) {
+					$this->CI->load->model('accounts_model');
+					$pgp = $this->CI->accounts_model->get_pgp_key($to['id']);
+					$content['message'] = $this->CI->gpg->encrypt($pgp['fingerprint'], $content['message']);
+					$encrypted = '1';
+			}		
+		}
+		// JSON encode the content array, and encrypt it if able.
+		$content = json_encode($content);
+		if($this->encrypt_private_messages)
+			$content = $this->CI->openssl->encrypt($content, $to['public_key']);
+		
+		// Encode the data before insertion.
+		$content = base64_encode($content);
+		$hash = $this->CI->general->unique_hash('messages','hash');
+	
+		$results = array('to' => $to['id'],
+						 'content' => $content,
+						 'hash' => $hash,
+						 'remove_on_read' => $remove_on_read,
+						 'encrypted' => $encrypted,
+						 'time' => time()
+						 );
+						 
+		return $results;
+	}
+	
+	/**
+	 * Prepare Output
+	 * 
+	 * This function prepares database responses into a parsable array.
+	 * Used to display the inbox, or a single message.
+	 * 
+	 * The content JSON string (which may be RSA encrypted) is base64 decoded
+	 * and decrypted if necessary.
+	 * $messages is an array containing all the messages. Loop through each
+	 * and prepare for output.
+	 * 
+	 * @param		array	$messages
+	 * @return		array
+	 */
+	public function prepare_output($messages = NULL) {
+		$this->CI->load->model('users_model');
+		$this->CI->load->model('accounts_model');
+		
+		// If there are no messages, return false.
+		if($messages == NULL)
+			return FALSE;
+			
+		// If messages are encrypted, load the private key.
+		if($this->encrypt_private_messages == TRUE)
+			$this->private_key = $this->CI->users_model->load_message_key('private');
+			
+		$results = array();			
+		// Loop through messages, decoding, decrypting the content.
+		foreach($messages as $message) {
+			$tmp = base64_decode($message['content']);
+			$content = ($this->encrypt_private_messages == TRUE) ? $this->CI->openssl->decrypt($tmp, $this->private_key, $this->current_user->message_password) : $tmp;
+			$content = json_decode($content);
+			$res = array('encrypted' => $message['encrypted'],
+						 'from' => $this->CI->accounts_model->get(array('id' => $content->from)),
+						 'hash' => $message['hash'],
+						 'id' => $message['id'],
+						 'message' => $content->message,
+						 'remove_on_read' => $message['remove_on_read'],
+						 'subject' => $content->subject,
+						 'time' => $message['time'],
+						 'time_f' => $this->CI->general->format_time($message['time']),
+						 'viewed' => $message['viewed']);
+			unset($message);
+			unset($content);
+			array_push($results, $res);
+			unset($res);
+		}
+		return $results;
+	}
+	
+};
